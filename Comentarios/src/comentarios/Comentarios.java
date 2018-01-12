a1f2ce4e921ca606382bb8b0956b4ebd8fe6683b
JorgePinargote <jorge.apif@gmail.com>
2018-01-11 23:55:01 -0500

    Mal olor: Comentarios, Tecnica de Refactoring: Extraer variable

---
+++ Comentarios/src/comentarios/Comentarios.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comentarios;
+
+/**
+ *
+ * @author Jorge Pinargote
+ */
+public class Comentarios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
