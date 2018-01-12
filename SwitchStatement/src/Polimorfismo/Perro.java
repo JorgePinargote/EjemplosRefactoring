82d201d9bb2859cac565cac3bde903617511e823
JorgePinargote <jorge.apif@gmail.com>
2018-01-11 23:39:54 -0500

    Mal olor: Switch Statement, Tecnica de Refactoring: Reestructurar usando Polimorfismo

---
+++ SwitchStatement/src/Polimorfismo/Perro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polimorfismo;
+
+/**
+ *
+ * @author Jorge Pinargote
+ */
+public class Perro implements Tipo {
+
+    @Override
+    public void SayWhat() {
+         System.out.println("Bark!");
+    }
+    
+}
